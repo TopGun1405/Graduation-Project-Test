90f886fb8d5e8e4a1dcc95fe5042a561cd8ba194
TopGun1405 <fstopgun0317@gmail.com>
2023-05-16 01:36:58 +0900

    test

---
+++ public/assets/js/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6KBLu8TQ4_2r20jaUj_7pSX2XMOzZLuY",
+    authDomain: "test-31487.firebaseapp.com",
+    databaseURL: "https://test-31487-default-rtdb.firebaseio.com",
+    projectId: "test-31487",
+    storageBucket: "test-31487.appspot.com",
+    messagingSenderId: "649597832416",
+    appId: "1:649597832416:web:e6683e6acdfba4fb959485",
+    measurementId: "G-2PWY8H0W19"
+};
+
+firebase.initializeApp(firebaseConfig);
